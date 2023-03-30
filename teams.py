2fc6ac1ec4ccaa3d9cfea74c3585c03720e3b463
Javier Cisneros <jvr.cisneros@gmail.com>
2023-03-30 13:12:45 -0600

    field player2

--- teams.py
+++ teams.py
@@ -1,14 +1,12 @@
 # -*- coding: utf-8 -*-
 from odoo import models,fields
 
-
 class coachs(models.Model):
     _name = 'helloworld.coachs'
     name = fields.Char(string='Nombre')
     photo = fields.Binary(string='Foto')
 
     _order = 'name'
-
 
 class teams(models.Model):
     _name = 'helloworld.teams'
@@ -18,5 +16,5 @@ class teams(models.Model):
     coach_id = fields.Many2one('helloworld.coachs',string='Coach')
     photo = fields.Binary(string='Foto')
     player_ids = fields.One2many('helloworld.players','team_id',string='Team')
-
+    player2_ids = fields.Many2many('helloworld.players',string='Jugadores')
     _order = 'name'
